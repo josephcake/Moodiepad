97e7773d4a095949fa9238127d70b7e56d8bf17f
Joseph <joseph8p@gmail.com>
2020-11-07 12:08:10 -0500

    added empty pages: login & home

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
-import {Switch} from 'react-router-dom'
+import {Login} from './pages/Login'
+import {Home} from './pages/Home'
 
 function App() {
+
+  //check token, if token => Home 
+  // else => Login
   return (  
     <div className="App">
       App
+      <Login>
+        
+      </Login>
     </div>
   );
 }
