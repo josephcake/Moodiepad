10937a0b8f103dfb0f23e6497bbf321f0263d495
21shield <21netaly@gmail.com>
2020-11-07 17:57:29 -0500

    loooooogin

--- src/App.js
+++ src/App.js
@@ -1,52 +1,22 @@
-<<<<<<< HEAD
 import "./App.scss";
 import React, { useState } from "react";
 import { Login } from "./pages/Login";
-import { Home } from "./pages/Home";
+import { Main } from "./pages/Main";
 import { Navigation } from "./containers/Navigation";
 import { Footer } from "./containers/Footer";
+import { NavigationProvider } from "./context/NavigationContext";
 
 function App() {
-  //check token, if token => Home
-  // else => Login
-  const [state, setState] = useState();
+  let login = true;
 
-  let login = false;
   return (
-    <div className="App">
-      <Navigation />
-      {login ? <Home /> : <Login />}
-      <Footer />
-    </div>
-=======
-import './App.scss';
-import {Login} from './pages/Login'
-import {Main} from './pages/Main'
-import {Navigation} from './containers/Navigation'
-import {Footer} from './containers/Footer'
-import {NavigationProvider} from './context/NavigationContext'
-
-function App() { 
-
-  let login = true
-
-
-  return (  
     <NavigationProvider>
       <div className="App">
-        <Navigation/>
-        {
-          login
-          ?
-          <Main/>
-          :
-          <Login/>
-        }  
-        <Footer/>    
+        <Navigation />
+        {login ? <Main /> : <Login />}
+        <Footer />
       </div>
     </NavigationProvider>
-
->>>>>>> master
   );
 }
 
